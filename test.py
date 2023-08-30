99782faa9cfbf0a2b228aca57a70546149f791f6
bop192 <124259758+bop192@users.noreply.github.com>
2023-08-30 13:45:02 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+import pyautogui as p
+import time as t
+clik = p.locateOnScreen('coccoc.png')
+p.doubleClick(clik)
+t.sleep(1)
+p.write('facebook.com',interval=0.15)
+p.press('enter')
+t.sleep(10)
+cik = p.locateOnScreen('post.png')
+p.click(cik)
+t.sleep(1)
+p.write('nguoi yeu toi chua co, co chi em yeu 100 nguoi ',interval=0.15)
+t.sleep(1)
+# p.hotkey('ctrl','enter')
+cliksend = p.locateOnScreen('send.png')
+p.doubleClick(cliks)
